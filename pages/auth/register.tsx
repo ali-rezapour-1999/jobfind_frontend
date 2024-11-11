32b1849689bfac557bbe00f475953440957d351d
ali-rezapour-1999 <ali.programer.1999@gmail.com>
2024-11-11 22:34:18 +0330

    create new components

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,58 +1,9 @@
 import Wrapper from "@/components/wrapper";
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { Button, Link, Typography, Stack, styled } from "@mui/material";
-import MuiCard from "@mui/material/Card";
+import { Button, Link, Typography } from "@mui/material";
 import AuthInput from "@/components/input";
-
-const Card = styled(MuiCard)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignSelf: "center",
-  width: "100%",
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: "auto",
-  borderRadius: "20px",
-  boxShadow:
-    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
-  [theme.breakpoints.up("sm")]: {
-    maxWidth: "450px",
-  },
-  ...theme.applyStyles("dark", {
-    background: "rgba(0,0,0,.3)",
-    boxShadow:
-      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
-  }),
-}));
-
-const SignUpContainer = styled(Stack)(({ theme }) => ({
-  height: "calc((1 - var(--template-frame-height, 0)) * 80dvh)",
-  minHeight: "100%",
-  padding: theme.spacing(2),
-  [theme.breakpoints.up("sm")]: {
-    padding: theme.spacing(4),
-  },
-  "&::before": {
-    content: '""',
-    display: "block",
-    position: "absolute",
-    zIndex: -1,
-    inset: 0,
-    backgroundImage:
-      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
-    backgroundRepeat: "no-repeat",
-    ...theme.applyStyles("dark", {
-      backgroundImage:
-        "radial-gradient(at 50% 50%, hsla(180, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
-    }),
-
-    ...theme.applyStyles("light", {
-      backgroundImage:
-        "radial-gradient(at 50% 50%, hsla(120, 100%, 85%, 0.5), hsl(220, 30%, 100%))",
-    }),
-  },
-}));
+import { CardContainer, Container } from "@/components/container";
 
 const Register = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -61,8 +12,8 @@ const Register = () => {
 
   return (
     <Wrapper>
-      <SignUpContainer direction="column" justifyContent="space-between">
-        <Card variant="outlined">
+      <Container direction="column" justifyContent="space-between">
+        <CardContainer variant="outlined">
           <Typography
             component="h2"
             variant="h2"
@@ -131,8 +82,8 @@ const Register = () => {
               </span>
             </Typography>
           </Box>
-        </Card>
-      </SignUpContainer>
+        </CardContainer>
+      </Container>
     </Wrapper>
   );
 };
