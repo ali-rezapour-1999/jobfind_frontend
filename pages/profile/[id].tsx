62f3d12c32889fe47ca8c219b7b9985898a1766e
ali-rezapour-1999 <ali.programer.1999@gmail.com>
2024-11-14 22:32:11 +0330

    set config for profile info

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,7 +1,22 @@
 import Wrapper from "@/components/wrapper";
 import React from "react";
+import { useRouter } from "next/router";
+import baseApi from "@/config/baseApi";
 
 const UserProfile = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  React.useEffect(() => {
+    if (id) {
+      const GetData = async () => {
+        const res = await baseApi.get(`/profile/profiles/${id}/`);
+        console.log(res.data);
+      };
+      GetData();
+    } else {
+      router.push("/");
+    }
+  }, [id, router]);
   return <Wrapper>userProfile</Wrapper>;
 };
 
